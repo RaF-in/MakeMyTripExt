ee6a047966e67b3d1b7647bacd09fb4b120890ce
Rafin Neaz <neazrafin@gmail.com>
2025-11-20 22:59:34 +0600

    Start implementing adapter service

---
+++ supplier-adapter-service/src/main/java/com/mmtext/supplieradapterservice/SupplierAdapterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mmtext.supplieradapterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierAdapterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupplierAdapterServiceApplication.class, args);
+    }
+
+}
---
+++ supplier-adapter-service/src/test/java/com/mmtext/supplieradapterservice/SupplierAdapterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmtext.supplieradapterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierAdapterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
